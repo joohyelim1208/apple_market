bfe752fcf53b39a99b756e9995cb348a8e31334c
jin <bdtjin00@gmail.com>
2025-12-29 21:33:48 +0900

    feat: detail-page

---
+++ lib/ui/detail_page/detail_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset("assets/images/AppleMarketLogo.webp", height: 30),
+      ),
+      body: Center(
+        child: Container()
+      ),
+    );
+  }
+}
